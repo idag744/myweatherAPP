9eca35145d16aa5c55c6046d7e3a369e79873f8c
Aida <aidagarfami@gmail.com>
2023-05-04 23:50:38 -0700

    trying to fix the bug

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-function search(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#city-input");
-  let h1 = document.querySelector("h1");
-  if (searchInput.value) {
-    h1.innerHTML = `${searchInput.value}`;
-  } else {
-    alert("please enter a valid city name...");
-  }
-}
-
-let searchCity = document.querySelector("#searchingForm");
-
-searchCity.addEventListener("submit", search);
-
 let apikey = "1f6442bbd1f73e321e5a2d48f43396d3";
-let apiUrl =
-  "http://api.openweathermap.org/data/2.5/weather?q=Sydney&units=metric&APPID=a2e49dcccda9de726c4b47b8edd7a944";
-
-let url =
-  "https://api.openweathermap.org/data/2.5/weather?q=London&appid={API key}";
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${response.data.name}&units=metric&APPID=${apikey}`;
 
 function showTemperature(response) {
   console.log(response);
-  let temp = document.querySelector("#temperature");
-  temp.innerHTML = response.data.main.temp;
+
+  info.innerHTML = response.data.main.temp;
+
+  let info = document.querySelector("#mainTemperature");
 }
 axios.get(`${apiUrl}&appid=${apikey}`).then(showTemperature);
+
+function search(event) {
+  event.preventDefault();
+
+  let city = document.querySelector("#city-input");
+
+  let h1 = document.querySelector("h1");
+  if (city.value) {
+    h1.innerHTML = `${city.value}`;
+  }
+}
+let city = response.data.main.name;
+searchCity.addEventListener("submit", search);
