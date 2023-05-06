eedb58d2322e244210d75baa97210182f189e6f4
Aida <aidagarfami@gmail.com>
2023-05-06 12:14:13 -0700

    defined searchcity function and handleSubmit function

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
-let apikey = "1f6442bbd1f73e321e5a2d48f43396d3";
+function search(city) {
+  let apikey = "dd13320e3c3ba6c3bc970f09d672c7de";
 
-let city = document.querySelector("#city-input").value;
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apikey}&units=metric`;
+  let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+  axios.get(apiurl).then(displayWeatherCondition);
+}
 
-function showTemperature(response) {
+function displayWeatherCondition(response) {
   console.log(response);
 
   document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#mainTemperature").innerHTML = math.round(
+  document.querySelector("#mainTemperature").innerHTML = Math.round(
     response.data.main.temp
   );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
 }
-axios.get(`${apiUrl}&appid=${apikey}`).then(showTemperature);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  search(city);
+}
+
+let searchInput = document.querySelector("#searchingForm");
+
+searchInput.addEventListener("submit", handleSubmit);
+
+search("New York");
