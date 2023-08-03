79a108e6435bcffa56e3558230bc28e2381f5186
Aida <aidagarfami@gmail.com>
2023-08-03 12:23:33 +0200

    added icon and updated the date

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0:${hours}`;
+  }
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0:${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
+}
 function search(city) {
   let apikey = "dd13320e3c3ba6c3bc970f09d672c7de";
 
@@ -18,6 +42,19 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute(
+    "alt",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
+  );
+  document.querySelector("#precipitation").innerHTML = Math.round();
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
 }
 
 function handleSubmit(event) {
